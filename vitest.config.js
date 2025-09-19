1d9229f7443805817fdad1d4a9f77de5727f1946
lucasmckeon <95000203+lucasmckeon@users.noreply.github.com>
2025-09-18 14:28:59 -1000

    try get test to work

--- vitest.config.js
+++ vitest.config.js
@@ -5,8 +5,7 @@ import path from 'path';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+    environment: 'node',
     include: ['**/*.{test,spec}.{ts,tsx}'],
   },
   resolve: {
